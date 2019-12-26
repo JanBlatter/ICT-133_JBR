772dab51a97a3e64f8cbc335771529512acaab9f
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2019-12-26 14:29:10 +0100

    exercice demoArray fait et ajout de logger 2

--- Exercices/Step 02 - Array/DemoArrays.php
+++ Exercices/Step 02 - Array/DemoArrays.php
@@ -20,11 +20,25 @@ $mostUsedBrowser = $browsers[2];
 
 // Replaces 'Edge' with 'Opera'
 //TODO - il vous faut coder ici
+$browsers[1] ='Opera';          // Remplaces 'Edge' in 'Opera'
+
 
 // Loop through an Indexed Array
 $arrLength = count($browsers);      // Assign the number of Elements of the Array '$browsers'
 $browsersList = "";
 //TODO - il vous faut coder ici
+for ($compteur =0;$compteur < $arrLength;$compteur++)       // Boucle qui affiche les valeurs du tableau indexé
+{
+    if ($compteur ==  0)    // Si le compteur est à 0 , affiche le tableau sans un tiret
+    {
+        $browsersList = $browsers[$compteur];
+    }
+    else {                              // affiche avec un tiret
+       $browsersList = $browsersList. ' - ' . $browsers[$compteur];
+    }
+}
+    echo $browsersList;
+    echo '<br>';
 
 //</editor-fold>
 
@@ -39,15 +53,28 @@ $contactInfo= array(
 
 // assign the city ("Yverdon") to $Home
 //TODO - il vous faut coder ici
+$Home = $contactInfo['City'];
 
 // Replaces 'John Doe' with 'Jane Doe'
 //TODO - il vous faut coder ici
+$contactInfo['name']='Jane Doe';
+
 
 // Loop through an Associated Array
 $completedContact="";
-foreach ($contactInfo as $element){
-    //TODO - il vous faut coder ici
+
+foreach ($contactInfo as $element){             // affiche les valeurs via les élements
+    if ($element == 'Jane Doe'){                // Si le pointeur est à jane Doe donc au début, affiche sans tiret
+        echo  $completedContact = $element;
+    }
+    else
+    {
+       echo  $completedContact = '-'. $element;        // Sinon affiche avec tiret , donc après jane doe
+    }
+
 }
+
+echo '<br>';
 //</editor-fold>
 
 //<editor-fold desc="Two-dimensional Array">
@@ -59,10 +86,11 @@ $people= array(
 );
 
 // assign "Joe" to $person
-//TODO - il vous faut coder ici
+$person = $people[2][0];
 
 // Replaces 'Bart' with 'Lisa'
-//TODO - il vous faut coder ici
+$people[1][2] = 'Lisa';
+
 
 
 // Loop through an Two-dimensional Array
@@ -71,4 +99,18 @@ $colLength = count($people[0]);     //Assign the number of Elements (columns) of
 $personList="";                     // Initialise the string $PersonList
 
 //TODO - il vous faut coder ici
+for ($row =0;$row <$rowLength;$row++)
+{
+    if ($row == 0)
+    {
+         $personList = $people[$row][$row];
+
+    }
+    for ($col = 0; $col < $colLength; $col++)
+    {
+         $personList = $personList . " - " . $people[$row][$col];
+    }
+
+}
+echo $personList;
 //</editor-fold>
